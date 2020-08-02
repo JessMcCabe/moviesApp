e44dd408d0aeb1f5682f88a49690c7309238d17a
Jessica McCabe <mccabejessica88@gmail.com>
2020-08-02 13:44:25 +0100

    Cast Member Details
    Bring Components together to display on one page.

---
+++ src/pages/castDetailsPage.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect}  from "react";
+import CastHeader from "../components/headerCast";
+import CastDetails from "../components/castDetails";
+import "./moviePage.css";
+
+const CastPage = props => {
+  const { id } = props.match.params;
+  const [cast, setCast] = useState(null);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/person/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+    ).then(res => res.json())    
+    .then(cast => {
+      setCast(cast);
+    });
+  }, [id]);
+  return (
+    <>
+      {cast ? (
+        <>
+          <CastHeader cast={cast} />
+          <div className="row">
+            <div className="col-3">
+              <img
+                src={
+                  cast.profile_path
+                    ? `https://image.tmdb.org/t/p/w500/${cast.profile_path}`
+                    : "./film-poster-placeholder.png"
+                }
+                className="movie"
+                alt={cast.name}
+              />
+            </div>
+            <div className="col-9">
+              <CastDetails cast={cast} />
+            </div>
+          </div>
+        </>
+        ) : (
+        <p>Waiting for cast details</p>
+      )}
+    </>
+  );
+};
+
+export default CastPage;
